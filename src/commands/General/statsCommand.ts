a22f65b014627d8f41d2834fedc4a6d221c2d887
Toby <40370496+DaatUserName@users.noreply.github.com>
2022-06-23 22:35:18 +0200

    stats command for hypixel

--- src/commands/General/statsCommand.ts
+++ src/commands/General/statsCommand.ts
@@ -1,22 +1,44 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Args, Command, CommandOptions } from '@sapphire/framework';
-import type { Message } from 'discord.js';
-import { HypixelAPI } from 'hypixel-api-v2';
+import { Command, CommandOptions } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+import { Client } from 'hypixel.ts';
 import { API_KEY } from '../../config';
-
+import mcapi from 'minecraft-lookup';
+import moment from 'moment';
 @ApplyOptions<CommandOptions>({
 	description: 'Hypixel stats',
 	name: 'stats',
 	fullCategory: ['General']
 })
 export default class statsCommand extends Command {
-	public async messageRun(message: Message, args: Args) {
-		const user: any = args.pick('string').catch(() => null);
+	public async messageRun(message: Message) {
+		
 
-		if (!user) return message.channel.send('Please enter a player name');
+		let client = new Client(API_KEY)
+		let player = await client.players.fetch('btsp');
+		let firstLogin = new Date(player.firstLogin);
+		let lastLogin = new Date(player.lastLogin);
+		const skin = await mcapi.skin('btsp');
 
-		const hypixel = await new HypixelAPI(API_KEY);
-		const player = await hypixel.player(user);
-		console.log(`${player.displayname} has ${player.networkExp} experience.`);
+
+		const embed = new MessageEmbed()
+			.setAuthor({ name: `User info for ${player.displayname}` })
+			.addFields(
+				{ name: 'Username', value: player.displayname, inline: true },
+				{ name: 'UUID', value: player.uuid, inline: true },
+				{ name: 'First login', value: `${moment(firstLogin).fromNow()}`, inline: true },
+				{ name: 'Last login', value: `${moment(lastLogin).fromNow()}`, inline: true },
+				{ name: 'Karma', value: `${player.karma}`, inline: true },
+				{ name: 'Network Exp', value: `${player.networkExp}`, inline: true },
+				{ name: 'Bedwars Games', value: `${player.stats.Bedwars.games_played_bedwars}`, inline: true },
+				{ name: 'Bedwars wins', value: `${player.stats.Bedwars.wins_bedwars}`, inline: true },
+				{ name: "Bedwars Kils", value: `${player.stats.Bedwars.kills_bedwars}`, inline: true },
+				{ name: 'Duels Games', value: `${player.stats.Duels.games_played_duels}`, inline: true },
+				{ name: 'Duels wins', value: `${player.stats.Duels.wins}`, inline: true },
+				{ name: "Duels Kils", value: `${player.stats.Duels.kills}`, inline: true},
+		)
+			.setThumbnail(`${skin.sideview}`)
+		return message.channel.send({ embeds: [embed] });
+				
 	}
 }
