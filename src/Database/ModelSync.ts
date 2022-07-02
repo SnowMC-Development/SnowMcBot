c5306d00b6150aebabaa2b08bc91fc52fcb2c531
Toby <40370496+DaatUserName@users.noreply.github.com>
2022-07-02 14:30:06 +0200

    stuff

--- src/Database/ModelSync.ts
+++ src/Database/ModelSync.ts
@@ -4,7 +4,6 @@ import { MarriageSchema } from './models/MarrySchema';
 import { SnipeModel } from './models/SnipeModel';
 import { GiveawayModel } from './models/GiveawayModel';
 import { GuildModel } from './models/GuildsModel';
-import { LevelModel } from './models/LevelModel';
 export const syncModels = () => {
 	ModerationModel.sync();
 	TicketSchema.sync();
@@ -12,5 +11,4 @@ export const syncModels = () => {
 	SnipeModel.sync();
 	GiveawayModel.sync();
 	GuildModel.sync();
-	LevelModel.sync();
 };
